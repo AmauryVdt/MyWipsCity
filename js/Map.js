68253cbde6edec80a5d11654fa691d4b26cf5aa9
AmauryVdt <amaury.vdt@gmail.com>
2020-11-05 11:56:39 +0100

    ajout du leader board et du score

--- js/Map.js
+++ js/Map.js
@@ -1,5 +1,15 @@
-window.onload = addPicture();
 var countDrag = 0;
+var countCommercePicture,
+    countMaisonPicture,
+    countAppartementPicture,
+    countEolienePicture,
+    countHydrauliquePicture,
+    countSolairePicture,
+    countEcolePicture,
+    countIndustriePicture,
+    countLacPicture,
+    countParcPicture = 1;
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -14,24 +24,29 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
     addPicture();
     countDrag = countDrag + 1;
-    //document.getElementById("scoreBloc").innerHTML = countDrag ;    A rajouter lorsque l'on veut afficher le score dans un bloc
     if (ev.target === document.getElementById("trash")) {
         var img = document.getElementById(data);
         img.parentNode.removeChild(img);
 
     }
+    updateScore();
+}
+
+function updateScore() {
+    document.getElementById("score").innerText = this.countDrag;
 }
 
 // Merci de ne pas juger le code à partir d'ici, j'avais la flemme de faire un beau code
 
-var countCommercePicture,
-    countMaisonPicture,
-    countAppartementPicture,
-    countEolienePicture,
-    countHydrauliquePicture,
-    countSolairePicture,
-    countEcolePicture,
-    countIndustriePicture  = 1;
+function border(id) {
+    // const div =document.getElementById(id)
+    // if (div.childElementCount > 0) {
+    //     div.style.border = "none";
+    // }
+    // else {
+    //     div.style.border = "1px dashed white;";
+    // }
+}
 
 function addPicture() {
     if (document.getElementById("commerce").childElementCount < 1) {
@@ -66,7 +81,7 @@ function addPicture() {
         img.src = "../images/immeuble.png";
         img.draggable = "true";
         img.width="150";
-        img.height="200";
+        img.height="150";
         this.addEventListener('dragstart', function() {drag(event)}, false);
 
         const div = document.getElementById("appartement");
@@ -137,5 +152,31 @@ function addPicture() {
         const div = document.getElementById("industrie");
         div.appendChild(img);
         countIndustriePicture++;
+    }
+    else if (document.getElementById("lac").childElementCount < 1) {
+        const img = document.createElement("img");
+        img.id = `maisonLac${countLacPicture}`;
+        img.src = "../images/usine.png";
+        img.draggable = "true";
+        img.width="150";
+        img.height="150";
+        this.addEventListener('dragstart', function() {drag(event)}, false);
+
+        const div = document.getElementById("lac");
+        div.appendChild(img);
+        countLacPicture++;
+    }
+    else if (document.getElementById("parc").childElementCount < 1) {
+        const img = document.createElement("img");
+        img.id = `maisonParc${countParcPicture}`;
+        img.src = "../images/parc.png";
+        img.draggable = "true";
+        img.width="150";
+        img.height="150";
+        this.addEventListener('dragstart', function() {drag(event)}, false);
+
+        const div = document.getElementById("parc");
+        div.appendChild(img);
+        countParPicture++;
     }
 }
